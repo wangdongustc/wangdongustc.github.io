b2d9eed923026af7251590e48007c0ee55b3df6f
Darren Wang <wangdong115@foxmail.com>
2016-06-10 21:26:33 -0400

    init

---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+/*var s_bak = 0;
+var s = 0;
+window.onscroll = function(){
+	var p = document.body.scrollTop;
+	var q = window.innerHeight;
+	s = p+q;
+	document.getElementById("logo").innerHTML=s;
+	document.getElementById("logo").innerHTML+=document.getElementById("backpic1").height;
+}
+*/
